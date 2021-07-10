4c03b2b1ab5c6a566f9f3b1dc9760146d6211d24
bohyunkang <bbohyun@kakao.com>
2021-07-10 21:31:52 +0900

    feat: Proxy 패키지를 활용한 CORS 이슈 해결

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,9 +3,7 @@ import axios from "axios";
 
 function LandingPage() {
 	useEffect(() => {
-		axios
-			.get("http://localhost:5000/api/hello")
-			.then((response) => console.log(response.data));
+		axios.get("/api/hello").then((response) => console.log(response.data));
 	}, []);
 
 	return <div>LandingPage</div>;
