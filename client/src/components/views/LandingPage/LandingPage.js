3bda5c138908fc1073c6c28b09e611d840732202
bohyunkang <bbohyun@kakao.com>
2021-07-10 21:17:22 +0900

    feat: axios 패키지를 활용한 api 호출

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/api/hello")
+			.then((response) => console.log(response.data));
+	}, []);
+
 	return <div>LandingPage</div>;
 }
 
--- server/index.js
+++ server/index.js
@@ -43,6 +43,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World! 안녕하세요!");
 });
 
+app.get("/api/hello", (req, res) => {
+	res.send("안녕하세요!!");
+});
+
 app.post("/api/users/register", (req, res) => {
 	// 회원 가입할 때 필요한 정보들을 client에서 가져와 데이터 베이스에 담는다.
 	const user = new User(req.body);
