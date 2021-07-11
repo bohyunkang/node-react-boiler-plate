92ededfd58302f070d2332feacabf8aba69d2add
bohyunkang <bbohyun@kakao.com>
2021-07-11 17:48:27 +0900

    feat: 회원가입 페이지 기능 구현

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_actions/user_action";
 
-function RegisterPage() {
-	return <div>RegisterPage</div>;
+function RegisterPage(props) {
+	const dispatch = useDispatch();
+	const [Email, setEmail] = useState("");
+	const [Name, setName] = useState("");
+	const [Password, setPassword] = useState("");
+	const [ConfirmPassword, setConfirmPassword] = useState("");
+
+	const onEmailHandler = (event) => {
+		setEmail(event.target.value);
+	};
+	const onNameHandler = (event) => {
+		setName(event.target.value);
+	};
+	const onPasswordHandler = (event) => {
+		setPassword(event.target.value);
+	};
+	const onConfirmPasswordHandler = (event) => {
+		setConfirmPassword(event.target.value);
+	};
+	const onSubmitHandler = (event) => {
+		event.preventDefault();
+
+		if (Password !== ConfirmPassword) {
+			return alert("비밀번호와 비밀번호 확인은 일치해야 합니다!");
+		}
+
+		let body = {
+			email: Email,
+			name: Name,
+			password: Password,
+		};
+
+		dispatch(registerUser(body)).then((response) => {
+			if (response.payload.success) {
+				props.history.push("/login");
+			} else {
+				alert("Failed to sign up!");
+			}
+		});
+	};
+
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				width: "100%",
+				height: "100vh",
+			}}
+		>
+			<form
+				style={{ display: "flex", flexDirection: "column" }}
+				onSubmit={onSubmitHandler}
+			>
+				<label>Email</label>
+				<input type="text" value={Email} onChange={onEmailHandler} />
+				<label>Name</label>
+				<input type="text" value={Name} onChange={onNameHandler} />
+				<label>Password</label>
+				<input type="password" value={Password} onChange={onPasswordHandler} />
+				<label>Confirm Password</label>
+				<input
+					type="password"
+					value={ConfirmPassword}
+					onChange={onConfirmPasswordHandler}
+				/>
+				<br />
+				<button type="submit">Register</button>
+			</form>
+		</div>
+	);
 }
 
 export default RegisterPage;
