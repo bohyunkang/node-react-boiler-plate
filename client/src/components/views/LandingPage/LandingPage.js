f880e87373fb66cbe2c238bdaa332efa94d8f95e
bohyunkang <bbohyun@kakao.com>
2021-07-11 17:55:38 +0900

    feat: 로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
 	useEffect(() => {
 		axios.get("/api/hello").then((response) => console.log(response.data));
 	}, []);
 
+	const onClickHandler = () => {
+		axios.get("/api/users/logout").then((response) => {
+			if (response.data.success) {
+				props.history.push("/login");
+			} else {
+				alert("로그아웃에 실패했습니다.");
+			}
+		});
+	};
 	return (
 		<div
 			style={{
@@ -17,6 +26,8 @@ function LandingPage() {
 			}}
 		>
 			<h2>시작 페이지</h2>
+
+			<button onClick={onClickHandler}>Sign out</button>
 		</div>
 	);
 }
