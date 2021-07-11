806249b4b0c3741d7f31e7abde737ac5430aec78
bohyunkang <bbohyun@kakao.com>
2021-07-11 17:20:21 +0900

    feat: 로그인 페이지 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,7 +6,19 @@ function LandingPage() {
 		axios.get("/api/hello").then((response) => console.log(response.data));
 	}, []);
 
-	return <div>LandingPage</div>;
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				width: "100%",
+				height: "100vh",
+			}}
+		>
+			<h2>시작 페이지</h2>
+		</div>
+	);
 }
 
 export default LandingPage;
